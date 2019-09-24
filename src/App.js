3c9b1278acc6a88756ab945000a92e7906a17098
Vidya Sagar <vidya@spectronn.com>
2019-09-24 12:02:40 -0400

    added functional component version too..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ApexRealTwoFunc from "./ApexRealTwoFunc";
 import ApexRealTwo from "./ApexRealTwo";
 function App() {
-  return (<div>
-	  	<div className="App">
-	  		<ApexRealTwo />
-	  	</div>
-	  </div>
+  return (
+    <div>
+      <div className="App">
+        <ApexRealTwoFunc />
+      </div>
+      <div className="App">
+        <ApexRealTwo />
+      </div>
+    </div>
   );
 }
 
